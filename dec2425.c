7b7549f861cdfc9376eb8b998ac0a2449e4e9956
Uday123455 <89934216+Uday123455@users.noreply.github.com>
2025-12-24 15:08:36 +0000

    started learning from neso academy some first few lecture exercises

---
+++ dec2425.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int value(){
+    return 5;
+}
+
+int main(){
+    int uday;
+    char sarangal= 'c';
+    char u[5]= "noob";
+    uday=value();
+    printf("%s \n",u);
+    printf("%d",uday);
+   return 'c';
+}
