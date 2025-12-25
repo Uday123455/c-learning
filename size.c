f183744714dda2a87e3109c84ed4011d085d37c3
Uday123455 <89934216+Uday123455@users.noreply.github.com>
2025-12-25 15:12:01 +0000

    to use the sizeof function

---
+++ size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// program to demonstrate the use of size of operator and showing the size of different data types
+int main(){
+    int uday;
+    printf("%d \n",sizeof(uday));
+    printf("%d \n",sizeof(int));
+    printf("%d \n",sizeof(long));
+    printf("%d \n",sizeof(short));
+    return 0;
+}
