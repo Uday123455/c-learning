d8d36bf423d01f894da4c4d366e3bdd551e42462
Uday123455 <89934216+Uday123455@users.noreply.github.com>
2025-12-26 06:04:43 +0000

    program where i use getchar and putchar

---
+++ getput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int c;
+   
+    while(getchar()!=EOF){
+        putchar(c);
+    }
+    int a=EOF;
+    char b=EOF;
+    printf("%d %c",a,b);
+    return 0;
+}
